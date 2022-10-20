57eb6234924e0b74896471473dc51be167d8ed51
Dezinha1 <debora.hidalgo-ext@viavarejo.com.br>
2022-10-20 14:55:52 -0300

    Fluxo E2E

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -47,10 +47,11 @@ Cypress.Commands.add('preCadastro' , (email , senha , nome ,sobrenome ) =>{
 Cypress.Commands.add('addProdutos' , (produto, tamanho, cor, quantidade) =>{
 
     cy.get('.product-block')
-        .contains('Lando Gym Jacket').click()
+        .contains(produto ).click()
         cy.get('.button-variable-item-' + tamanho).click()
         cy.get('.button-variable-item-' + cor    ).click()
         cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
 
 })
+
